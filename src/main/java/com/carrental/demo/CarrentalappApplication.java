a66ec2712839a9c62383b41088d2349b68d26aa2
moreno <konradpstrongowski@gmail.com>
2023-04-27 18:48:47 +0200

    first commit

---
+++ src/main/java/com/carrental/demo/CarrentalappApplication.java
@@ -0,0 +1,13 @@
+package com.carrental.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalappApplication.class, args);
+	}
+
+}
