bccc2f665e443627999eb0850950e6520248ce72
moreno <konradpstrongowski@gmail.com>
2023-05-10 18:19:29 +0200

    feature (service) #1

---
+++ src/main/java/com/carrental/demo/service/_branch/BranchService.java
@@ -0,0 +1,6 @@
+package com.carrental.demo.service._branch;
+
+import com.carrental.demo.entity.Branch;
+
+public interface BranchService {
+}
---
+++ src/main/java/com/carrental/demo/service/_branch/BranchServiceImpl.java
@@ -0,0 +1,23 @@
+package com.carrental.demo.service._branch;
+
+import com.carrental.demo.entity.Branch;
+import com.carrental.demo.repository.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BranchServiceImpl{
+
+    private final BranchRepository branchRepository;
+
+    @Autowired
+    public BranchServiceImpl(BranchRepository branchRepository) {
+        this.branchRepository = branchRepository;
+    }
+
+    public Branch addBranch() {
+        branchRepository.save()
+    }
+
+
+}
